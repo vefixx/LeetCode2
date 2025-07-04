0723fefaf28722a22b3cbf3fefd83797418f6d79
vefixx <vef1xx@yandex.ru>
2025-07-04 13:19:29 +0500

    task191, task7

---
+++ Task191/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace Task191;
+
+class Program
+{
+    public int HammingWeight(int n)
+    {
+        int count = 0;
+
+        foreach (var item in Convert.ToString(n, 2))
+        {
+            if (item == '1')
+                count++;
+        }
+
+        return count;
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new Program().HammingWeight(2147483645));
+    }
+}
---
+++ Task7/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task7;
+
+class Program
+{
+    public int Reverse(int x)
+    {
+        string xStr = x.ToString();
+        string reversedStr;
+
+        if (x < 0)
+        {
+            char[] charrArray = xStr.Substring(1, xStr.Length-1).ToCharArray();
+            Array.Reverse(charrArray);
+            reversedStr = "-" + new string(charrArray);
+        }
+        else
+        {
+            char[] charrArray = xStr.ToCharArray();
+            Array.Reverse(charrArray);
+            reversedStr = new string(charrArray);
+        }
+        
+        if (!int.TryParse(reversedStr, out _))
+        {
+            return 0;
+        }
+        
+        return int.Parse(reversedStr);
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new Program().Reverse(-2147483648));
+    }
+    
+}
