bb009a4911417616d1ad2d7862bfe8e6e3287d7b
vefixx <vef1xx@yandex.ru>
2025-07-04 23:50:11 +0500

    task27

---
+++ Task27/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task27;
+
+class Program
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        int countToAdd = 0;
+        int k = 0;
+        int[] resNums = { };
+
+        foreach (var num in nums)
+        {
+            if (num == val)
+            {
+                countToAdd++;
+            }
+            else
+            {
+                Array.Resize(ref resNums, resNums.Length+1);
+                resNums[^1] = val;
+                k++;
+            }
+        }
+        
+        // Замена удаленных элементов на "_"
+        // for (int i = 0; i < countToAdd; i++)
+        // {
+        //     Array.Resize(ref resNums, resNums.Length+1);
+        //     resNums[^1] = val;
+        // }
+        
+        return k;
+    }
+    
+    static void Main(string[] args)
+    {
+        // https://leetcode.com/problems/remove-element/
+        Console.WriteLine(new Program().RemoveElement([0,1,2,2,3,0,4,2], 2));
+    }
+}
